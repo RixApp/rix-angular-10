9b9406cdf190959f8a4dcee232f1579144e8f14b
Giovanni Delgado <giovanniyatze@gmail.com>
2021-09-17 20:00:10 -0500

    Add board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
